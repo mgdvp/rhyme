ea7c8521adbe6f6033918cab64e076a9a973f48d
Məhəmməd B. <113066556+mgdvp@users.noreply.github.com>
2025-10-26 21:32:25 +0400

    Getting api key from Vercel backend

--- src/useApiKey.js
+++ src/useApiKey.js
@@ -1,30 +1,22 @@
-// useApiKey.js
-
 import { useState, useEffect } from 'react';
 
 const useApiKey = () => {
   const [apiKey, setApiKey] = useState('');
   const [apiKeySet, setApiKeySet] = useState(false);
 
-  const getApiKey = () => {
-    fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        grant_type: 'client_credentials',
-        client_id:  process.env.REACT_APP_SPOTIFY_CLIENT_ID,
-        client_secret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setApiKey(data.access_token);
-        const expirationTime = new Date().getTime() + data.expires_in * 1000;
-        localStorage.setItem('apiKey', data.access_token);
-        localStorage.setItem('expirationTime', expirationTime.toString());
-      });
+  const getApiKey = async () => {
+    try {
+      const res = await fetch('/api/getSpotifyToken');
+      const data = await res.json();
+      setApiKey(data.access_token);
+
+      const expirationTime = new Date().getTime() + data.expires_in * 1000;
+      localStorage.setItem('apiKey', data.access_token);
+      localStorage.setItem('expirationTime', expirationTime.toString());
+      setApiKeySet(true);
+    } catch (err) {
+      console.error("Failed to fetch Spotify token", err);
+    }
   };
 
   useEffect(() => {
@@ -33,11 +25,9 @@ const useApiKey = () => {
     const currentTime = new Date().getTime();
 
     if (storedApiKey && expirationTime && currentTime < expirationTime) {
-      // API key exists in localStorage and has not expired
       setApiKey(storedApiKey);
       setApiKeySet(true);
     } else {
-      // API key doesn't exist or has expired, fetch a new one
       getApiKey();
     }
   }, []);
